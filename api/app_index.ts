7b08295df9bca1178133f59927b72d979273a64f
Gabriele Genta <gabriele.genta@gmail.com>
2023-01-08 21:16:00 +0100

    Comment as to why ranking can be null

--- api/app_index.ts
+++ api/app_index.ts
@@ -17,6 +17,7 @@ type Details = {
     name: ExchangeName;
     weight: number;
     impact: number;
+    // This is `null` if ranking is above 200 (don't have data after that, but it doesn't matter much).
     ranking: number | null;
 };
 
